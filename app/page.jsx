41698f03791ef2993345d161bad9be828e7f7c22
tejas1113 <tejasmore1104@gmail.com>
2023-03-01 11:23:26 +0530

    Added producers

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,20 @@
 export default async function Home() {
+  const res = await fetch(`https://api.jikan.moe/v4/producers`);
+  const { data } = await res.json();
   return (
     <main>
       <h1>Welcome to Super Anime</h1>
       <p>One Stop spot for all trending animes and mangas for you</p>
+      <h1>Producers :</h1>
+      {data.map((person) => (
+        <div>
+          <img
+            src={person.images.jpg.image_url}
+            alt="producer"
+            style={{ height: 200 }}
+          />
+        </div>
+      ))}
     </main>
   );
 }
