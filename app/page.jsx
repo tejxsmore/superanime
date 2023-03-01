3136608b1289d192063203b639856b35ce6507a4
tejas1113 <tejasmore1104@gmail.com>
2023-03-01 06:16:43 +0530

    Fetch Successfull

--- app/page.jsx
+++ app/page.jsx
@@ -1,31 +1,26 @@
 import Anime from "./Anime";
 
 export default async function Home() {
-  // try {
-  //   const res = await fetch(`https://kitsu.io/api/edge/anme`);
-  //   if (!res.ok) {
-  //     throw new Error("Data fetch Unsuccessfull");
-  //   }
-  //   const data = await res.json();
-  //   return (
-  //     <div>
-  //       {data.data.map((anime) => (
-  //         <Anime
-  //           key={anime.id}
-  //           id={anime.id}
-  //           title={anime.attributes.canonicalTitle}
-  //           description={anime.attributes.description}
-  //           img={anime.attributes.posterImage.large}
-  //         />
-  //       ))}
-  //     </div>
-  //   );
-  // } catch (err) {
-  //   throw new Error(`${err.message}`);
-  // }
-  return (
-    <main>
-      <h1>Hello Tejas !</h1>
-    </main>
-  );
+  try {
+    const res = await fetch(`https://kitsu.io/api/edge/anime`);
+    if (!res.ok) {
+      throw new Error("Data fetch Unsuccessfull");
+    }
+    const data = await res.json();
+    return (
+      <div>
+        {data.data.map((anime) => (
+          <Anime
+            key={anime.id}
+            id={anime.id}
+            title={anime.attributes.canonicalTitle}
+            description={anime.attributes.description}
+            img={anime.attributes.posterImage.large}
+          />
+        ))}
+      </div>
+    );
+  } catch (err) {
+    throw new Error(`${err.message}`);
+  }
 }
