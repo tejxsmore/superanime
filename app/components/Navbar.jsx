844da53d2f5024a7d2ab11ad22cbae12c67782d8
tejas1113 <tejasmore1104@gmail.com>
2023-03-02 10:43:17 +0530

    Commit

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,6 +4,9 @@ export default function Navbar() {
   return (
     <div>
       <Link href="/">Home</Link>
+      <Link href="/anime">Anime</Link>
+      <Link href="/manga">Manga</Link>
+      <Link href="/characters">Characters</Link>
     </div>
   );
 }
