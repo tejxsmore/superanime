37877df9fb260a32d6f0df2d16bdcdebefd26781
tejxsmore <tejasmore1104@gmail.com>
2023-03-11 14:42:54 +0530

    Added Navbar and Footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,3 +1,28 @@
 export default function Footer() {
-  return <div></div>;
+  const year = new Date().getFullYear();
+  return (
+    <div className="mt-10">
+      <div className="flex justify-center items-center">
+        <div>
+          <p className="text-light text-lg">{`Copyright ${year} ©️ Super Anime`}</p>
+        </div>
+      </div>
+      <div className="text-light m-4 flex flex-col justify-center items-center">
+        <div>
+          <a
+            href="https://github.com/tejxsmore/superanime"
+            className="bg-bgpurp px-6 py-2 m-2 rounded-md"
+          >
+            Github
+          </a>
+          <a
+            href="https://linkedin.com/in/tejxsmore"
+            className="bg-bgpurp px-5 py-2 m-2 rounded-md"
+          >
+            LinkedIn
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
         <img src="/logo.png" alt="logo" className="h-12" />
       </a>
       <div className="p-2">
-        <button className="bg-button px-5 py-2 rounded-md  text-bg text-md font-semibold hover:bg-light hover:text-dark">
+        <button className="bg-bgpurp px-5 py-2 rounded-md  text-light text-md font-semibold hover:bg-light hover:text-bg hover:delay-75">
           SignIn
         </button>
       </div>
